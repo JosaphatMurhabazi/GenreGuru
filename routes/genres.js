8a1b13e5a3b77eecd87c2ca41fd457ed2696944c
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-01-18 08:33:28 +0100

    Setting up genre controller

--- routes/genres.js
+++ routes/genres.js
@@ -1,60 +1,17 @@
 const express = require('express');
-const Joi = require('joi');
+const {
+  getGenre,
+  getAllGenres,
+  createGenre,
+  deleteGenre,
+  updateGenre,
+} = require('../controllers/genres');
 const router = express.Router();
 
-const genres = [
-  { id: 1, name: 'Action' },
-  { id: 2, name: 'Horror' },
-  { id: 3, name: 'Romance' },
-];
-
-router.get('/', (req, res) => {
-  res.send(genres);
-});
-
-router.get('/:id', (req, res) => {
-  const genre = genres.find((genre) => genre.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).json('The genre with the given id was not found');
-  res.send(genre);
-});
-
-router.post('/', (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).json(error.details[0].message);
-
-  const genre = { id: genres.length + 1, name: req.body.name };
-  genres.push(genre);
-  res.status(200).json(genre);
-});
-router.put('/:id', (req, res) => {
-  const genre = genres.find((genre) => genre.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).json('The genre with the given ID was not found');
-
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).json(error.details[0].message);
-
-  genre.name = req.body.name;
-  res.status(200).json(genre);
-});
-
-router.delete('/:id', (req, res) => {
-  const genre = genres.find((genre) => genre.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).json('The genre with the given ID  was not found');
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-  res.status(200).json(genre);
-});
-
-function validateGenre(genre) {
-  const schema = Joi.object({
-    name: Joi.string().min(5).required(),
-  });
-
-  return schema.validate(genre);
-}
+router.get('/', getAllGenres);
+router.get('/:id', getGenre);
+router.post('/', createGenre);
+router.put('/:id', updateGenre);
+router.delete('/:id', deleteGenre);
 
 module.exports = router;
